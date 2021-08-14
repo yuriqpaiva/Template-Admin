e05b954d88671130e037256285e16b6224020ed4
yuriqpaiva <yuriqpaiva@gmail.com>
2021-08-14 13:31:31 -0300

    Integração Firebase
    Feita integração com firebase e vincular usuário do Google

---
+++ src/components/template/AvatarUsuario.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
+import useAuth from '../../data/hook/useAuthData'
+
+interface AvatarUsuarioProps {
+    className?: string
+}
+
+export default function AvatarUsuario(props: AvatarUsuarioProps) {
+
+    const { usuario } = useAuth()
+
+    return (
+        <Link href="/perfil" passHref>
+            <img
+                src={usuario?.imagemUrl ?? '/images/avatar.svg'}
+                alt="Avatar do usuário"
+                className={`
+                h-10 w-10 rounded-full cursor-pointer 
+                ${props.className}
+                `}
+            />
+
+        </Link>
+    )
+}
