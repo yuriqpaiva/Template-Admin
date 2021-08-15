3ad812f3894a1a131ba6b546b0741084e0c39160
yuriqpaiva <yuriqpaiva@gmail.com>
2021-08-15 08:53:53 -0300

    Avatar usuário e Cookies
    Implementação do Avatar padrão de usuário e Cookies para salvar o usuário quando a página der refresh

--- src/components/template/AvatarUsuario.tsx
+++ src/components/template/AvatarUsuario.tsx
@@ -20,7 +20,6 @@ export default function AvatarUsuario(props: AvatarUsuarioProps) {
                 ${props.className}
                 `}
             />
-
         </Link>
     )
 }
--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,7 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import firebase from "../../firebase/config";
 import Usuario from '../../model/Usuario'
 import route from 'next/router'
+import Cookies from 'js-cookie'
 
 interface AuthContextProps {
     usuario: Usuario
@@ -24,21 +25,51 @@ async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuar
     }
 }
 
+function gerenciarCookie(logado: boolean) {
+    if (logado) {
+        Cookies.set('admin-template-auth', logado, {
+            expires: 7
+        })
+    } else {
+        Cookies.remove('admin-template-auth')
+    }
+}
+
 export function AuthProvider(props) {
 
     const [usuario, setUsuario] = useState<Usuario>(null)
+    const [carregando, setCarregando] = useState(true)
+
+    // Função que dá set nos cookies e no usuário, recebendo como parâmetro
+    // o resp.user que as requisições retornarem
+    async function configurarSessao(usuarioFirebase) {
+        if (usuarioFirebase?.email) {
+            const usuario = await usuarioNormalizado(usuarioFirebase)
+            setUsuario(usuario)
+            gerenciarCookie(true)
+            setCarregando(false)
+            return usuario.email
+        } else {
+            setUsuario(null)
+            gerenciarCookie(false)
+            setCarregando(false)
+            return false
+        }
+    }
 
     async function loginGoogle() {
-
         const resp = await firebase.auth().signInWithPopup(
             new firebase.auth.GoogleAuthProvider()
         )
-        if (resp.user?.email) {
-            const usuario = await usuarioNormalizado(resp.user);
-            setUsuario(usuario);
-            route.push('/')
-        }
+        configurarSessao(resp.user)
+        route.push('/')
     }
+
+    useEffect(() => {
+        const cancelar = firebase.auth().onIdTokenChanged(configurarSessao)
+        return () => cancelar() // quando o componente for desmontado, `cancelar` não será mais chamado 
+    }, [])
+
 
     return (
         <AuthContext.Provider value={{
