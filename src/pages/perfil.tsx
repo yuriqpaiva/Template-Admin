b298759a5d8c3154537c4fc0f633479361ded78a
yuriqpaiva <yuriqpaiva@gmail.com>
2021-08-18 15:47:38 -0300

    Dados usuário perfil

--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -1,12 +1,23 @@
 import Layout from "../components/template/Layout"
-import useAppData from "../data/hook/useAppData"
+import Image from "next/dist/client/image"
+import useAuth from "../data/hook/useAuthData"
+import AvatarUsuario from "../components/template/AvatarUsuario"
 
 export default function Perfil() {
+
+  const {usuario } = useAuth()
 
   return (
     <Layout titulo='Perfil do Usuário'
       subtitulo='Administre as suas informações de usuário'>
-        <h1>Perfil do Usuário</h1>
+        <div className="flex mt-5">
+          <AvatarUsuario/>
+          <div className="mx-2">
+            <h2>{usuario?.nome}</h2>
+            <a href={`mailto: ${usuario?.email}`} type="email" className="underline hover:text-gray-300">{usuario?.email}</a>
+          </div>
+        </div>
+        <span></span>
     </Layout>
   )
 }
